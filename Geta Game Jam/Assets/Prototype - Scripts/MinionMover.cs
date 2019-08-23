4a9d582c47a2392829b1bfdc9bb513bc6228c183
Aden Webb <adenwebb21@gmail.com>
2019-08-24 00:29:07 +0100

    added player which can spin and shoot minions

---
+++ Geta Game Jam/Assets/Prototype - Scripts/MinionMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionMover : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Geta Game Jam/Assets/Prototype - Scripts/PlayerRotator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotator : MonoBehaviour
+{
+    private Vector3 mousePosition;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+    }
+}
