f0753b59d5ef75da76ebf7e3be5d5320b3e00d2e
Aden Webb <adenwebb21@gmail.com>
2019-08-24 14:50:18 +0100

    Enemy Spawning and Minion movement
    First fraft of the enemy spawning from random portals, minions approach correct portal

---
+++ Geta Game Jam/Assets/Prototype - Scripts/EnemyMover.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    private float enemySpeed = 1f;
+    private Rigidbody2D rb;
+
+    private GameObject closestMinion;
+    private GameObject player;
+
+    private bool isFighting = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+        enemySpeed = Random.Range(0.5f, 1.2f) * enemySpeed;
+    }
+
+    private void Update()
+    {
+        if(!isFighting)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, enemySpeed * Time.deltaTime);
+        } 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isFighting = true;
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isFighting = false;
+    }
+}
