ee8983ca10420dfe5ba6d16a34f700cd292f69e0
Jack Brewer <j.brewer@hotmail.co.uk>
2019-08-24 03:17:07 +0100

    Actually fixed problems xD

--- Geta Game Jam/Assets/Prototype - Scripts/MinionMover.cs
+++ Geta Game Jam/Assets/Prototype - Scripts/MinionMover.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 public class MinionMover : MonoBehaviour
 {
     public float speed;
+
+    private float MinionSpeed = 1f;
     private Rigidbody2D rb;
+    private GameObject closestEnemy;
+    private GameObject targetPortal;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
-        rb.velocity = transform.up * speed;
+        MinionSpeed = Random.Range(0.9f, 2f) * MinionSpeed;
+        FindClosestEnemy();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, closestEnemy.transform.position, MinionSpeed * Time.deltaTime);
+    }
+    private void FindClosestEnemy()
+    {
+        float minDistance = Mathf.Infinity;
+        Vector3 currentPos = transform.position;
+        foreach (GameObject enemy in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            Debug.Log("Enemy Found");
+            float distance = Vector3.Distance(enemy.transform.position, currentPos);
+            if (distance < minDistance)
+            {
+                closestEnemy = enemy;
+                minDistance = distance;
+            }
+        }
     }
 }
